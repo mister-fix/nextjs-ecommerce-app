cbb727c38b2bd57ec1b2d8a0eb25ed7a733345c4
Stephen <hellostephenwm@gmail.com>
2025-07-22 18:30:47 -0400

    chore: replace h1 with tensell logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
+import Image from 'next/image';
+
 export default function Home() {
 	return (
-		<div className="font-primary h-screen w-screen flex items-center justify-center content-center">
-			<h1 className="text-3xl">Hello World</h1>
+		<div className="font-primary h-screen w-screen flex items-center justify-center content-center bg-neutral-100 dark:bg-neutral-900">
+			<Image
+				src="/tensell.svg"
+				alt="Tensell logo"
+				width={175}
+				height={85}
+				draggable="false"
+				className="animate-bounce"
+			/>
 		</div>
 	);
 }
